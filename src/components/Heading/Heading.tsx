46ddc59af0637682e049cac7b1f17d6cdcd29f01
satvik9373 <113889674+satvik9373@users.noreply.github.com>
2024-07-06 12:54:00 +0530

    aa

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -2,13 +2,22 @@
 import React, { useState, useEffect } from 'react';
 
 const styles = [
-  'font-bold',
-  'italic',
-  'underline',
-  'font-bold italic',
-  'font-bold underline',
-  'italic underline',
-  'font-bold italic underline',
+  'font-montserrat',
+  'font-poppins',
+  'font-bold font-montserrat',
+  'font-bold font-poppins',
+  'italic font-montserrat',
+  'italic font-poppins',
+  'underline font-montserrat',
+  'underline font-poppins',
+  'font-bold italic font-montserrat',
+  'font-bold italic font-poppins',
+  'font-bold underline font-montserrat',
+  'font-bold underline font-poppins',
+  'italic underline font-montserrat',
+  'italic underline font-poppins',
+  'font-bold italic underline font-montserrat',
+  'font-bold italic underline font-poppins',
   // Add more styles if needed
 ];
 
@@ -22,7 +31,7 @@ const Header: React.FC = () => {
   useEffect(() => {
     const intervalId = setInterval(() => {
       setStyleIndex((prevIndex) => (prevIndex + 1) % styles.length);
-    }, 1000); // Change style every 2 seconds
+    }, 1000); // Change style every 1 second
 
     // Clear interval on component unmount
     return () => clearInterval(intervalId);
@@ -30,14 +39,13 @@ const Header: React.FC = () => {
 
   return (
     <div className="flex justify-center items-center">
-      <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+      <h1 className="max-w-4xl text-5xl md:text-6xl lg:text-7xl">
         <span
           className={`text-black cursor-pointer ${styles[styleIndex]}`}
           onClick={handleStyleChange}
         >
-          Razominer
-        </span>{' '}
-        your leads scrapping powerhouse from web
+          Razominer</span>{' '}
+ your leads scrapping powerhouse from web
       </h1>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ const config = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
+  ],
   prefix: "",
   theme: {
     container: {
@@ -18,6 +18,10 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
